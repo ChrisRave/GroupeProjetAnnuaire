bbcede732b4dbf7db84f962676190f490a55d8ef
ChrisRave <ravelonary_lalaina@live.fr>
2022-06-28 10:21:57 +0200

    Méthode ajouter stagiaire + Lanceur binaire

--- Projet Annuaire/src/fr/isika/cda18/projet1/entites/ArbreBinaire.java
+++ Projet Annuaire/src/fr/isika/cda18/projet1/entites/ArbreBinaire.java
@@ -10,89 +10,79 @@ import java.util.Collections;
 import java.util.List;
 import java.util.TreeSet;
 
-
 public class ArbreBinaire {
-	
 
 	static List<Stagiaire> stagiaires = new ArrayList<>();
-	
-	 Noeud racine; 
-	
-public  void importFichier(){
-		
+
+	Noeud racine;
+
+	public void importFichier() {
 
 		try {
 			FileReader fr = new FileReader("src/mesFichiers/STAGIAIRES_V2.DON");
 			BufferedReader br = new BufferedReader(fr);
-			
-			
+
 			while (br.ready()) {
-				Stagiaire stagiaire = new Stagiaire(null, null,null,null,null);
+				Stagiaire stagiaire = new Stagiaire(null, null, null, null, null);
 				stagiaire.setNom(br.readLine());
 				stagiaire.setPrenom(br.readLine());
 				stagiaire.setDepartement(br.readLine());
 				stagiaire.setPromotion(br.readLine());
 				stagiaire.setAnnee(br.readLine());
 				br.readLine();
-				stagiaires.add(stagiaire); 
-				
+				stagiaires.add(stagiaire);
 
 			}
 			System.out.println(stagiaires);
 			br.close();
-			fr.close(); 
+			fr.close();
 
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-	
+
 	}
 
-
-			
-public void ajouterRacine(Noeud racineAjouter) throws IOException {
-	try {
-		RandomAccessFile raf = new RandomAccessFile("src/mesFichiers/listeStagiaires.bin", "rw");
-		if (raf.length() == 0) {
-			this.racine = racineAjouter;
-			this.racine.ecritureBinaire();
-			raf.close();
-		} else {
-			raf.seek(0);
-			// ajouter Noeud récursif
-		}
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	}
-
-
-	
-////séparer dans nouvelle méthode 				
+	public void ajouterRacine(Noeud racineAjouter) throws IOException {
+		
+		RandomAccessFile raf ; 
+		
+			if (LanceurBinaire.raf.length() == 0) {
+				this.racine = racineAjouter;
+				this.racine.ecritureBinaire();
+				LanceurBinaire.raf.close();
+			} else {
+				LanceurBinaire.raf.seek(0);
+				this.ajouterStagiaire(new Noeud(null, -1, -1));
 			}
-//			else if (this.racine.getStagiaire().getNom().compareTo(noeudAAjouter.getStagiaire().getNom()) > 0) {
-//				if (this.racine.getFilsGauche() == -1) {
-//					
-//					this.filsGauche()++; // taille d'un fichier divisé par la taille du noeud, variable static pour Noeud (taille stagiaire + taille fils gauche et fils droit), l'écriture sur le fils g et fils d se fait dans fichier bin
-//					this.noeudAAjouter.ecritureBinaire();
-//					this.noeudAAjouter.lectureBinaire();
-//				} else {
-//					this.filsGauche.ajouterNoeud(noeudAAjouter);
-//				}
-//			} else {
-//				if (this.racine.getFilsDroit() == -1) {
-//					
-//					this.filsDroit++; 
-//					this.noeudAAjouter.ecritureBinaire();
-//					this.noeudAAjouter.lectureBinaire();
-//				} else {
-//					this.filsDroit.ajouterNoeud(noeudAAjouter);
-//				}
-//			}
-//		}
-//
-//	}
 
+	}
+	public void ajouterStagiaire(Noeud stagiaireAjouter) throws IOException {
+
+		RandomAccessFile raf;
+
+		if (this.racine.getStagiaire().getNom().compareTo(stagiaireAjouter.getStagiaire().getNom()) > 0) {
+
+			if (this.racine.getFilsGauche() == -1) {
+				LanceurBinaire.raf.seek(raf.getFilePointer() - 8);
+				LanceurBinaire.raf.writeInt((int) (raf.length() / Stagiaire.TAILLE_OBJET_OCTET));
+				LanceurBinaire.raf.seek(raf.length());
+				this.ajouterStagiaire(stagiaireAjouter);
+			} else {
+				this.racine.getFilsGauche().ajouterStagiaire(stagiaireAjouter);
+			}
+
+		} else {
+			if (this.racine.getFilsDroit() == -1) {
+				LanceurBinaire.raf.seek(raf.getFilePointer() - 4);
+				LanceurBinaire.raf.writeInt((int) (raf.length() / Stagiaire.TAILLE_OBJET_OCTET));
+				LanceurBinaire.raf.seek(raf.length());
+				LanceurBinaire.raf.stagiaireAjouter.ecritureBinaire();
+			} else {
+				this.racine.getFilsDroit().ajouterStagiaire(stagiaireAjouter);
+			}
+		}
+
+	}
 
 }
-
-
--- Projet Annuaire/src/fr/isika/cda18/projet1/entites/Noeud.java
+++ Projet Annuaire/src/fr/isika/cda18/projet1/entites/Noeud.java
@@ -4,14 +4,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public class Noeud implements InterfaceTailles{
+public class Noeud implements InterfaceTailles {
 	private Stagiaire stagiaire;
 	private int filsGauche;
 	private int filsDroit;
 
-	
-	
-	//Constructeur
+	// Constructeur
 	public Noeud(Stagiaire stagiaire, int filsGauche, int filsDroit) {
 
 		this.stagiaire = stagiaire;
@@ -22,8 +20,6 @@ public class Noeud implements InterfaceTailles{
 	public Stagiaire getStagiaire() {
 		return stagiaire;
 	}
-
-
 
 	public void setStagiaire(Stagiaire stagiaire) {
 		this.stagiaire = stagiaire;
@@ -45,61 +41,59 @@ public class Noeud implements InterfaceTailles{
 		this.filsDroit = filsDroit;
 	}
 
-	//Méthodes spécifiques
+	// Méthodes spécifiques
 	public void ecritureBinaire() throws IOException {
 
 		try {
 			RandomAccessFile raf = new RandomAccessFile("src/mesFichiers/listeStagiaires.bin", "rw");
-			raf.writeChars(this.stagiaire.agrandirNom());
-			raf.writeChars(this.stagiaire.agrandirPrenom());
-			raf.writeChars(this.stagiaire.getDepartement());
-			raf.writeChars(this.stagiaire.agrandirPromo());
-			raf.writeChars(this.stagiaire.getAnnee());
-			raf.writeInt(this.filsDroit); 
-			raf.writeInt(this.filsGauche); 
+			LanceurBinaire.raf.writeChars(this.stagiaire.agrandirNom());
+			LanceurBinaire.raf.writeChars(this.stagiaire.agrandirPrenom());
+			LanceurBinaire.raf.writeChars(this.stagiaire.getDepartement());
+			LanceurBinaire.raf.writeChars(this.stagiaire.agrandirPromo());
+			LanceurBinaire.raf.writeChars(this.stagiaire.getAnnee());
+			LanceurBinaire.raf.writeInt(this.filsDroit);
+			LanceurBinaire.raf.writeInt(this.filsGauche);
 			raf.close();
 		} catch (IOException e) {
-			
 			e.printStackTrace();
 		}
 
-		
 	}
 
 	public void lectureBinaire() throws IOException {
 
-		RandomAccessFile raf;
+	
 		try {
-			raf = new RandomAccessFile("src/mesFichiers/listeStagiaiares.bin", "rw");
-			raf.seek(0);
+			RandomAccessFile raf = new RandomAccessFile("src/mesFichiers/listeStagiaiares.bin", "rw");
+			LanceurBinaire.raf.seek(0);
 			for (int j = 0; j < raf.length() / Stagiaire.TAILLE_OBJET_OCTET; j++) {
 				String nom = "";
 				String prenom = "";
 				String departement = "";
 				String promo = "";
 				String annee = "";
-				int filsGauche = -1 ; 
-				int filsDroit = -1; 
+				int filsGauche = -1;
+				int filsDroit = -1;
 				for (int i = 0; i < Stagiaire.TAILLE_NOM; i++) {
-					nom += raf.readChar();
+					nom += LanceurBinaire.raf.readChar();
 				}
 				for (int i = 0; i < Stagiaire.TAILLE_PRENOM; i++) {
-					prenom += raf.readChar();
+					prenom += LanceurBinaire.raf.readChar();
 				}
 				for (int i = 0; i < Stagiaire.TAILLE_DEPARTEMENT; i++) {
-					departement += raf.readChar();
+					departement += LanceurBinaire.raf.readChar();
 				}
 				for (int i = 0; i < Stagiaire.TAILLE_PROMO; i++) {
-					promo += raf.readChar();
+					promo += LanceurBinaire.raf.readChar();
 				}
 				for (int i = 0; i < Stagiaire.TAILLE_ANNEE; i++) {
-					annee += raf.readChar();
+					annee += LanceurBinaire.raf.readChar();
 				}
-				for (int i = 0; i< Stagiaire.TAILLE_OBJET_OCTET; i++) {
-					filsGauche+= raf.readInt(); 
+				for (int i = 0; i < Stagiaire.TAILLE_OBJET_OCTET; i++) {
+					filsGauche += LanceurBinaire.raf.readInt();
 				}
-				for (int i = 0; i< Stagiaire.TAILLE_OBJET_OCTET; i++) {
-					filsDroit+= raf.readInt(); 
+				for (int i = 0; i < Stagiaire.TAILLE_OBJET_OCTET; i++) {
+					filsDroit += LanceurBinaire.raf.readInt();
 				}
 				System.out.println("Nom :" + nom.trim() + "\t Prenom :" + prenom.trim() + "\t Departement :"
 						+ departement + "\t Promotion :" + promo.trim() + "\t Annee : " + annee);
@@ -111,7 +105,6 @@ public class Noeud implements InterfaceTailles{
 			e.printStackTrace();
 		}
 
-	}	
-	
+	}
 
 }
